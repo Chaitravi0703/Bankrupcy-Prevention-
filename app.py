b052d8f24cd3d1ebcffd6551a3683cab156c002e
Chaitravi Angane <106433189+Chaitravi0703@users.noreply.github.com>
2022-08-16 21:07:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,82 @@
+<p align="center">
+  <img width=60% height=250px src="https://user-images.githubusercontent.com/94888819/184377349-cb8b12ff-fd01-49a3-adfb-f7149e9023e8.png">
+</p>
+
+**Bankruptcy is a legal process through which people or other entities who cannot repay debts to creditors may seek relief from some or all of their debts.**
+<br><br>
+# `Information about columns:`
+<img src="https://user-images.githubusercontent.com/94888819/184372144-36e1c42a-11a5-4a9d-8c59-4bca56714cba.png" width=60% align="left" alt="img1">
+<img align="right" alt="bank1"  height="160" width=30% src = "https://user-images.githubusercontent.com/94888819/184385810-6e750ced-a29d-43f9-a6c1-5af1052b8b91.gif"> 
+<br><br><br><br><br><br><br>
+
+<img src="https://user-images.githubusercontent.com/94888819/184372753-a3e2179a-db7b-48fb-b5ac-ee6c3570eec0.png" width=60% align="right" alt="img2">
+
+<img src="https://user-images.githubusercontent.com/94888819/184372977-34e9e2a9-08f1-4488-8651-6ea1df26a444.png" width=60% align="left" alt="img3">
+
+<img src="https://user-images.githubusercontent.com/94888819/184373102-9cd30bb3-be2d-4cce-ba81-e570705c9997.png" width=60% align="right" alt="img4">
+<img align="left" alt="bank2"  height="180" width=30% src = "https://user-images.githubusercontent.com/94888819/184384288-1195ff20-50c9-4adb-acc1-2183e715af43.gif"> 
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<img src="https://user-images.githubusercontent.com/94888819/184373332-8468e08c-3189-471e-a896-e76e33ce3e2b.png" width=60% align="left" alt="img5">
+<img src="https://user-images.githubusercontent.com/94888819/184373491-5d8eccfd-c67e-47ef-be6e-5037e6b76ba7.png" width=60% align="right" alt="img6">
+<br><br><br><br><br><br><br><br><br><br><br><br>
+
+# `Dataset:`
+
+<img align="left" width=60% height=300px src="https://user-images.githubusercontent.com/94888819/184480249-68928f62-5f1c-4064-b0fc-46a642ba5163.png">
+<img align="right" width=30% src="https://user-images.githubusercontent.com/94888819/184480701-40d9aea5-de5d-407c-a2c8-37a939227da1.png">
+<br><br><br><br><br><br><br><br><br><br><br><br>
+
+# `Insights`
+
+<p align="left">
+  <img width=50% src="https://user-images.githubusercontent.com/94888819/184482320-c982e3d6-b2ae-4b54-8ccb-534e242c48b9.png">
+</p>
+<p align="right">
+  <img width=50% src="https://user-images.githubusercontent.com/94888819/184483238-a4dda147-0f35-43eb-ab83-ae0aa24e3f9d.png">
+</p>
+<p align="left">
+  <img width=50% src="https://user-images.githubusercontent.com/94888819/184484637-f2539c89-88ce-4a05-a0d3-e3f125360115.png">
+</p>
+<p align="right">
+  <img width=50% src="https://user-images.githubusercontent.com/94888819/184483649-21cf7d03-5e68-4884-a705-6fdd574db06b.png">
+</p>
+<p align="left">
+  <img width=50% src="https://user-images.githubusercontent.com/94888819/184483854-46a155a4-0fc0-4eef-acf3-21579b4072f9.png">
+</p>
+<p align="right">
+  <img width=50% src="https://user-images.githubusercontent.com/94888819/184484021-e7d8609c-aa87-41f4-96be-af2e9194d8ce.png">
+</p>
+
+# `Model Building`
+
+<img align="left" width=30% src="https://user-images.githubusercontent.com/94888819/184488032-e982c5ed-856d-4284-80ba-0b95cbddecfb.png">
+<img align="right" width=65% alt="SS" src="https://user-images.githubusercontent.com/94888819/184487964-831b5233-4241-4e21-8d25-838c330a4769.png">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+# `Model Deployment` 
+
+<p align="center">
+  <img width=90% src="https://user-images.githubusercontent.com/94888819/184490720-d4d7f909-8778-4901-8e61-93fb991100f7.png">
+</p>
+<p align="center">
+  <img width=90% src="https://user-images.githubusercontent.com/94888819/184491146-6bfc9737-067c-4272-bee7-4757e776cb30.png">
+</p>
+<p align="center">
+  <img width=90% src="https://user-images.githubusercontent.com/94888819/184491601-70265eac-6ca8-4b0e-951b-88825a9cd151.png">
+</p>
+
+# `Group Members`
+
+<img src="https://user-images.githubusercontent.com/94888819/184492866-91ba459f-9881-4c3a-92b1-10fd10078948.gif" width="350px" align="right" alt="logo" />
+
+This project is part of Data Science internship in [Excelr](https://learn.excelr.com/login).
+Member | Email | LinkedIn |
+| --- | --- | --- |
+| **Our Mentor: Neha RAmchandani** | [💌](NR@gmail.com) | [Click Me](https://www.linkedin.com/) |
+| **Omkar Bhagwat** | [💌](https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSGMSqpLlKHdJncbTZTdtLhpXqgQSwHCFpfrjfZtHKSSfSsbndnmvKSTbBncQRzXzfTqwgKn) | [Click Me](https://www.linkedin.com/in/omkar-bhagwat-64b103230) |
+| **Manju Kiran** | [💌](https://mail.google.com/mail/u/0/#inbox?compose=CllgCJTNqLRFszLcsmmlCThKhwwtPfgTksfpBHzXLnnwjJkSbwStDZHKDMrTdHZPZHrppSzWCZL) |[Click Me](https://in.linkedin.com/) |
+| **Chitravi Angane** | [💌](https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlQHVftJQMlZKhBJjnFWzKGhwxgckCSxwhZWwqZGXCtLNtbnNsZTMjnpmQgcWgVGldKfZQ) | [Click Me](https://www.linkedin.com/in/chaitravi-angane-556b29241/) |
+| **Yogita Mishra** | [💌](https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlQHVftJQMlZKhBJjnFWzKGhwxgckCSxwhZWwqZGXCtLNtbnNsZTMjnpmQgcWgVGldKfZQ) | [Click Me](https://in.linkedin.com/in/yogita-mishra-8487b5161?trk=people-guest_people_search-card&original_referer=https%3A%2F%2Fwww.linkedin.com%2F) |
+| **Amrut Vishwaroo** | [💌](https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlQHVftJQMlZKhBJjnFWzKGhwxgckCSxwhZWwqZGXCtLNtbnNsZTMjnpmQgcWgVGldKfZQ) | [Click Me](https://www.linkedin.com/in/amrut-vishwaroop-0ab946232/) |
+| **Akarsh Bhasi** | [💌](https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRlQHVftJQMlZKhBJjnFWzKGhwxgckCSxwhZWwqZGXCtLNtbnNsZTMjnpmQgcWgVGldKfZQ) | [Click Me](https://www.linkedin.com/in/akarshbhasi/) | 
